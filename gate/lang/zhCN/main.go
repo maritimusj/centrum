fcc190f8cf178eb82d4e111c0098c01f42d7457d
maritimusj <6300119@qq.com>
2020-02-24 14:48:26 +0800

    chore:清除暂时不需要的字符串

--- gate/lang/zhCN/main.go
+++ gate/lang/zhCN/main.go
@@ -45,11 +45,12 @@ var (
 		lang.MenuRoleSystemSettingsTitle: "系统设定",
 		lang.MenuRoleSysLogsTitle:        "系统日志",
 
-		lang.UserDefaultRoleDesc: "用户默认角色",
+		lang.UserDefaultRoleDesc: "",
 
-		lang.RoleSystemAdminDesc:        "系统默认账户，也是最高权限用户！ ",
-		lang.RoleOrganizationAdminDesc:  "组织机构管理员，是指定组织机构中初始用户。",
-		lang.RoleGuestDesc:              "一般游客账户，只有查看权限。",
+		lang.RoleSystemAdminDesc:       "",
+		lang.RoleOrganizationAdminDesc: "",
+		lang.RoleGuestDesc:             "",
+
 		lang.MenuRoleGalleryDesc:        "查看设备实时状态",
 		lang.MenuRoleDevicesDesc:        "可以对设备进行管理，添加删除和编辑设备",
 		lang.MenuRoleAlertDesc:          "可以查看管理警报信息",
@@ -61,6 +62,7 @@ var (
 
 		lang.ResourceConfigBaseDetailTitle: "",
 		lang.ResourceConfigBaseDetailDesc:  "",
+
 		lang.ResourceConfigBaseUpdateTitle: "",
 		lang.ResourceConfigBaseUpdateDesc:  "",
 
@@ -80,92 +82,125 @@ var (
 		lang.ResourceListDesc:       "",
 		lang.ResourceDetailDesc:     "",
 
-		lang.ResourceMyProfileDetailTitle:    "",
-		lang.ResourceMyProfileDetailDesc:     "",
-		lang.ResourceMyProfileUpdateTitle:    "",
-		lang.ResourceMyProfileUpdateDesc:     "",
-		lang.ResourceMyPermTitle:             "",
-		lang.ResourceMyPermDesc:              "",
-		lang.ResourceMyPermMultiTitle:        "",
-		lang.ResourceMyPermMultiDesc:         "",
-		lang.ResourceUserLogListTitle:        "",
-		lang.ResourceUserLogListDesc:         "",
-		lang.ResourceUserLogDeleteTitle:      "",
-		lang.ResourceUserLogDeleteDesc:       "",
-		lang.ResourceDeviceLogListTitle:      "",
-		lang.ResourceDeviceLogListDesc:       "",
-		lang.ResourceDeviceLogDeleteTitle:    "",
-		lang.ResourceDeviceLogDeleteDesc:     "",
-		lang.ResourceEquipmentLogListTitle:   "",
-		lang.ResourceEquipmentLogListDesc:    "",
+		lang.ResourceMyProfileDetailTitle: "",
+		lang.ResourceMyProfileDetailDesc:  "",
+
+		lang.ResourceMyProfileUpdateTitle: "",
+		lang.ResourceMyProfileUpdateDesc:  "",
+
+		lang.ResourceMyPermTitle: "",
+		lang.ResourceMyPermDesc:  "",
+
+		lang.ResourceMyPermMultiTitle: "",
+		lang.ResourceMyPermMultiDesc:  "",
+
+		lang.ResourceUserLogListTitle: "",
+		lang.ResourceUserLogListDesc:  "",
+
+		lang.ResourceUserLogDeleteTitle: "",
+		lang.ResourceUserLogDeleteDesc:  "",
+
+		lang.ResourceDeviceLogListTitle: "",
+		lang.ResourceDeviceLogListDesc:  "",
+
+		lang.ResourceDeviceLogDeleteTitle: "",
+		lang.ResourceDeviceLogDeleteDesc:  "",
+
+		lang.ResourceEquipmentLogListTitle: "",
+		lang.ResourceEquipmentLogListDesc:  "",
+
 		lang.ResourceEquipmentLogDeleteTitle: "",
 		lang.ResourceEquipmentLogDeleteDesc:  "",
 
-		lang.ResourceUserListTitle:   "用户列表",
-		lang.ResourceUserListDesc:    "查看用户列表",
-		lang.ResourceUserCreateTitle: "创建用户",
-		lang.ResourceUserCreateDesc:  "创建新用户",
-		lang.ResourceUserUpdateTitle: "编辑用户",
-		lang.ResourceUserUpdateDesc:  "修改用户资料",
-		lang.ResourceUserDetailTitle: "查看资料",
-		lang.ResourceUserDetailDesc:  "查看用户资料",
-		lang.ResourceUserDeleteTitle: "删除用户",
-		lang.ResourceUserDeleteDesc:  "删除用户",
+		lang.ResourceUserListTitle: "",
+		lang.ResourceUserListDesc:  "",
 
-		lang.ResourceRoleListTitle:   "角色列表",
-		lang.ResourceRoleListDesc:    "查看角色列表",
-		lang.ResourceRoleCreateTitle: "创建角色",
-		lang.ResourceRoleCreateDesc:  "创建新的角色",
-		lang.ResourceRoleUpdateTitle: "编辑角色",
-		lang.ResourceRoleUpdateDesc:  "修改角色权限",
-		lang.ResourceRoleDetailTitle: "查看角色",
-		lang.ResourceRoleDetailDesc:  "查看角色详情",
-		lang.ResourceRoleDeleteTitle: "删除角色",
-		lang.ResourceRoleDeleteDesc:  "删除角色",
+		lang.ResourceUserCreateTitle: "",
+		lang.ResourceUserCreateDesc:  "",
 
-		lang.ResourceGroupListTitle:   "分组列表",
-		lang.ResourceGroupListDesc:    "查看全部设备分组",
-		lang.ResourceGroupCreateTitle: "创建分组",
-		lang.ResourceGroupCreateDesc:  "创建新的设备分组",
-		lang.ResourceGroupDetailTitle: "查看分组",
-		lang.ResourceGroupDetailDesc:  "查看分组详情",
-		lang.ResourceGroupUpdateTitle: "编辑分组",
-		lang.ResourceGroupUpdateDesc:  "修改分组信息",
-		lang.ResourceGroupDeleteTitle: "删除分级",
-		lang.ResourceGroupDeleteDesc:  "删除设备分组",
+		lang.ResourceUserUpdateTitle: "",
+		lang.ResourceUserUpdateDesc:  "",
 
-		lang.ResourceDeviceListTitle:   "设备列表",
-		lang.ResourceDeviceListDesc:    "查看设备列表",
-		lang.ResourceDeviceCreateTitle: "添加设备",
-		lang.ResourceDeviceCreateDesc:  "添加新设备",
-		lang.ResourceDeviceDetailTitle: "查看设备",
-		lang.ResourceDeviceDetailDesc:  "查看设备详情",
-		lang.ResourceDeviceUpdateTitle: "编辑设备",
-		lang.ResourceDeviceUpdateDesc:  "修改设备详情",
-		lang.ResourceDeviceDeleteTitle: "删除设备",
-		lang.ResourceDeviceDeleteDesc:  "移除一个设备",
+		lang.ResourceUserDetailTitle: "",
+		lang.ResourceUserDetailDesc:  "",
 
-		lang.ResourceMeasureListTitle:   "点位列表",
-		lang.ResourceMeasureListDesc:    "查看点位列表",
-		lang.ResourceMeasureCreateTitle: "添加点位",
-		lang.ResourceMeasureCreateDesc:  "添加一个新的点位",
-		lang.ResourceMeasureDetailTitle: "查看点位",
-		lang.ResourceMeasureDetailDesc:  "查看点位详情",
-		lang.ResourceMeasureUpdateTitle: "编辑点位",
-		lang.ResourceMeasureUpdateDesc:  "修改点位",
-		lang.ResourceMeasureDeleteTitle: "删除点位",
-		lang.ResourceMeasureDeleteDesc:  "删除点位",
+		lang.ResourceUserDeleteTitle: "",
+		lang.ResourceUserDeleteDesc:  "",
 
-		lang.ResourceEquipmentListTitle:   "自定义设备列表",
-		lang.ResourceEquipmentListDesc:    "查看自定义设备列表",
-		lang.ResourceEquipmentCreateTitle: "创建自定义设备",
-		lang.ResourceEquipmentCreateDesc:  "创建一个新的自定义设备",
-		lang.ResourceEquipmentDetailTitle: "查看自定义设备",
-		lang.ResourceEquipmentDetailDesc:  "查看自定义设备详情",
-		lang.ResourceEquipmentUpdateTitle: "编辑自定义设备",
-		lang.ResourceEquipmentUpdateDesc:  "修改自定义设备",
-		lang.ResourceEquipmentDeleteTitle: "删除自定义设备",
-		lang.ResourceEquipmentDeleteDesc:  "删除一个自定设备",
+		lang.ResourceRoleListTitle: "",
+		lang.ResourceRoleListDesc:  "",
+
+		lang.ResourceRoleCreateTitle: "",
+		lang.ResourceRoleCreateDesc:  "",
+
+		lang.ResourceRoleUpdateTitle: "",
+		lang.ResourceRoleUpdateDesc:  "",
+
+		lang.ResourceRoleDetailTitle: "",
+		lang.ResourceRoleDetailDesc:  "",
+
+		lang.ResourceRoleDeleteTitle: "",
+		lang.ResourceRoleDeleteDesc:  "",
+
+		lang.ResourceGroupListTitle: "",
+		lang.ResourceGroupListDesc:  "",
+
+		lang.ResourceGroupCreateTitle: "",
+		lang.ResourceGroupCreateDesc:  "",
+
+		lang.ResourceGroupDetailTitle: "",
+		lang.ResourceGroupDetailDesc:  "",
+
+		lang.ResourceGroupUpdateTitle: "",
+		lang.ResourceGroupUpdateDesc:  "",
+
+		lang.ResourceGroupDeleteTitle: "",
+		lang.ResourceGroupDeleteDesc:  "",
+
+		lang.ResourceDeviceListTitle: "",
+		lang.ResourceDeviceListDesc:  "",
+
+		lang.ResourceDeviceCreateTitle: "",
+		lang.ResourceDeviceCreateDesc:  "",
+
+		lang.ResourceDeviceDetailTitle: "",
+		lang.ResourceDeviceDetailDesc:  "",
+
+		lang.ResourceDeviceUpdateTitle: "",
+		lang.ResourceDeviceUpdateDesc:  "",
+
+		lang.ResourceDeviceDeleteTitle: "",
+		lang.ResourceDeviceDeleteDesc:  "",
+
+		lang.ResourceMeasureListTitle: "",
+		lang.ResourceMeasureListDesc:  "",
+
+		lang.ResourceMeasureCreateTitle: "",
+		lang.ResourceMeasureCreateDesc:  "",
+
+		lang.ResourceMeasureDetailTitle: "",
+		lang.ResourceMeasureDetailDesc:  "",
+
+		lang.ResourceMeasureUpdateTitle: "",
+		lang.ResourceMeasureUpdateDesc:  "",
+
+		lang.ResourceMeasureDeleteTitle: "",
+		lang.ResourceMeasureDeleteDesc:  "",
+
+		lang.ResourceEquipmentListTitle: "",
+		lang.ResourceEquipmentListDesc:  "",
+
+		lang.ResourceEquipmentCreateTitle: "",
+		lang.ResourceEquipmentCreateDesc:  "",
+
+		lang.ResourceEquipmentDetailTitle: "",
+		lang.ResourceEquipmentDetailDesc:  "",
+
+		lang.ResourceEquipmentUpdateTitle: "",
+		lang.ResourceEquipmentUpdateDesc:  "",
+
+		lang.ResourceEquipmentDeleteTitle: "",
+		lang.ResourceEquipmentDeleteDesc:  "",
 
 		lang.ResourceDeviceStatusTitle:     "",
 		lang.ResourceDeviceDataTitle:       "",
@@ -191,16 +226,20 @@ var (
 		lang.ResourceEquipmentCHValueDesc:    "",
 		lang.ResourceEquipmentStatisticsDesc: "",
 
-		lang.ResourceStateListTitle:   "自定义点位列表",
-		lang.ResourceStateListDesc:    "查看自定义点位列表",
-		lang.ResourceStateCreateTitle: "添加自定义点位",
-		lang.ResourceStateCreateDesc:  "添加一个新的自定义点位",
-		lang.ResourceStateDetailTitle: "查看自定义点位",
-		lang.ResourceStateDetailDesc:  "查看片定义点位详情",
-		lang.ResourceStateUpdateTitle: "编辑自定义点位",
-		lang.ResourceStateUpdateDesc:  "修改自定义点位",
-		lang.ResourceStateDeleteTitle: "删除自定义点位",
-		lang.ResourceStateDeleteDesc:  "删除一个自定义点位",
+		lang.ResourceStateListTitle: "",
+		lang.ResourceStateListDesc:  "",
+
+		lang.ResourceStateCreateTitle: "",
+		lang.ResourceStateCreateDesc:  "",
+
+		lang.ResourceStateDetailTitle: "",
+		lang.ResourceStateDetailDesc:  "",
+
+		lang.ResourceStateUpdateTitle: "",
+		lang.ResourceStateUpdateDesc:  "",
+
+		lang.ResourceStateDeleteTitle: "",
+		lang.ResourceStateDeleteDesc:  "",
 
 		lang.AlarmListTitle:    "",
 		lang.AlarmListDesc:     "",
@@ -220,10 +259,11 @@ var (
 		lang.CommentDeleteTitle: "",
 		lang.CommentDeleteDesc:  "",
 
-		lang.ResourceLogListTitle:      "系统日志",
-		lang.ResourceLogListDesc:       "查看系统日志",
-		lang.ResourceLogDeleteTitle:    "删除系统日志",
-		lang.ResourceLogDeleteDesc:     "清空系统日志记录",
+		lang.ResourceLogListTitle: "",
+		lang.ResourceLogListDesc:  "",
+
+		lang.ResourceLogDeleteTitle:    "",
+		lang.ResourceLogDeleteDesc:     "",
 		lang.ResourceLogLevelListTitle: "",
 		lang.ResourceLogLevelListDesc:  "",
 
