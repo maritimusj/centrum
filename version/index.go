6d4c761467aa587223a3ec27d6024b63f691432e
maritimusj <6300119@qq.com>
2020-04-13 14:43:00 +0800

    chore:*

--- version/index.go
+++ version/index.go
@@ -2,7 +2,7 @@ package version
 
 var (
 	EdgeVersion   = "2.0.16"
-	EdgeBuildDate = "20200410003"
+	EdgeBuildDate = "20200413004"
 	GateVersion   = "2.0.17"
-	GateBuildDate = "20200410006"
+	GateBuildDate = "20200413006"
 )
