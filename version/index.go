28d95d410b01277d4c96d5b986fd49a69c2053e2
maritimusj <6300119@qq.com>
2020-05-07 09:27:35 +0800

    chore:last alarm

--- version/index.go
+++ version/index.go
@@ -2,7 +2,7 @@ package version
 
 var (
 	EdgeVersion   = "2.0.16"
-	EdgeBuildDate = "20200413004"
-	GateVersion   = "2.0.17"
-	GateBuildDate = "20200413006"
+	EdgeBuildDate = "20200507001"
+	GateVersion   = "2.0.18"
+	GateBuildDate = "20200307009"
 )
